b6dbf622af896a910690ecd8b31a4fc108be7af8
tetsuhi <sntigg@gmail.com>
2025-01-25 01:32:19 +0100

    bubble collision

--- GlobalJam25/Assets/Scripts/BubbleTimer.cs
+++ GlobalJam25/Assets/Scripts/BubbleTimer.cs
@@ -13,4 +13,9 @@ public class BubbleTimer : MonoBehaviour
     {
         Destroy(gameObject);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
 }
